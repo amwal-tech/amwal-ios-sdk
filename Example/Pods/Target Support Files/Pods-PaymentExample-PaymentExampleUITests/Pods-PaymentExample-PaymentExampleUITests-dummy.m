c9fdc03210f3207d3221fbbcff26e3c969888193
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2023-07-24 23:49:08 +0300

    removing example

--- Example/Pods/Target Support Files/Pods-PaymentExample-PaymentExampleUITests/Pods-PaymentExample-PaymentExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PaymentExample_PaymentExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PaymentExample_PaymentExampleUITests
-@end
--- Example/Pods/Target Support Files/Pods-PaymentExampleTests/Pods-PaymentExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PaymentExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_PaymentExampleTests
-@end
