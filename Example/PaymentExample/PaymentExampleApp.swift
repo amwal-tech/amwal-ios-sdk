9a8c3e3e762c7599053f9b51630136a81dcdc876
Ahmed Elshaer <ahmed3elshaer@gmail.com>
2023-07-24 19:36:36 +0300

    adding project example

---
+++ Example/PaymentExample/PaymentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentExampleApp.swift
+//  PaymentExample
+//
+//  Created by Ahmed Elshaer on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct PaymentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/Pods/Target Support Files/Pods-PaymentExample-PaymentExampleUITests/Pods-PaymentExample-PaymentExampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PaymentExample_PaymentExampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_PaymentExample_PaymentExampleUITests
+@end
