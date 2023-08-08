4375794f0a46214aa561e715489f8fbedf095fbb
Fares <Mahmoudfares9394@gmail.com>
2023-08-08 11:57:09 +0300

    implement Example Project

--- Example/Pods/Target Support Files/AmwalPayment/AmwalPayment-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AmwalPayment : NSObject
-@end
-@implementation PodsDummy_AmwalPayment
-@end
---
+++ Example/Pods/Target Support Files/Pods-AmwalPaymentExample/Pods-AmwalPaymentExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AmwalPaymentExample : NSObject
+@end
+@implementation PodsDummy_Pods_AmwalPaymentExample
+@end
